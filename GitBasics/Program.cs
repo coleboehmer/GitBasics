0fa757d8c3a9dc0de37e06a5f0d2d430bdccbdc0
Cole Boehmer <ColeBo@Magenic.com>
2019-02-28 21:19:46 -0600

    Initial Check in GetBasics project

---
+++ GitBasics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
